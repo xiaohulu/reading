b4e31aa2dd1c4cd14af5c663239352cfe0fc27ab
xiaohulu <idocument@qq.com>
2019-07-05 21:08:49 +0800

    提炼函数(无局部变量)

---
+++ 重构(第二版)/001_before.js
@@ -0,0 +1,27 @@
+// 传入客户账单，打印出客户的欠款信息
+function printOwing(invoice) {
+    // 声明变量，未支付金额
+    let outstanding = 0;
+
+    // 打印横幅
+    console.log("***********************");
+    console.log("**** Customer Owes ****");
+    console.log("***********************");
+
+    // 计算未支付金额
+    for(const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+
+    // 设置到期日
+    const today = Clock.today;
+    invoice.dueDate = new Date(
+        today.getFullYear(), 
+        today.getMonth(), 
+        today.getDate() + 30);
+
+    // 打印详情
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
